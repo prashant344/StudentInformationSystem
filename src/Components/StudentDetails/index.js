7388e7817c31e79786f3ac3be3b097641ad78213
prashant <prashant.ranjan@tesco.com>
2018-06-28 23:16:46 +0530

    changes to realig elements

--- src/Components/StudentDetails/index.js
+++ src/Components/StudentDetails/index.js
@@ -10,7 +10,7 @@ const StudentDetails = ({location}) => {
     for(var key in marks){
         data.push({text:key,value:marks[key]})
     }
-    const margin = {top: 20, right: 0, bottom: 30, left: 40};
+    const margin = {top: 20, right: 20, bottom: 30, left: 20};
     return(
         <div>
             <TopNavigation />
@@ -22,19 +22,19 @@ const StudentDetails = ({location}) => {
                 <p>Name: {location.state.Name}</p>
                 <p>Class: {location.state.Class}</p>
                 <p>Roll No.: {location.state.RollNo}</p>
-                <p>Marks in s1: {location.state.Marks.s1}</p>
-                <p>Marks in s2: {location.state.Marks.s2}</p>
-                <p>Marks in s3: {location.state.Marks.s3}</p>
-                {location.state.Marks.s4!==undefined ? (<p>Marks in s4: {location.state.Marks.s4}</p>): null}
-                <div style={{width: '100%'}}> 
-                    <BarChart ylabel='Marks'
-                    width={500}
-                    height={500}
+                {data.map((val)=>{return (
+                    <p key={val.text}>Marks in {val.text}: {val.value}</p>
+                )})}
+                <p></p>  
+            </div>
+            </div>
+            <div> 
+                <BarChart ylabel='Marks'
+                height= {400}
+                    width={400}
                     margin={margin}
                     data={data}
                     colorByLabel={false}/>
-                </div>
-            </div>
             </div>
         </div>
     )
